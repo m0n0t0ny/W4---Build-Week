ddaeae459a0905b67bbd57b97b0feb44bbc4e49d
Antonio Bertuccio <anto.bertu@gmail.com>
2023-10-18 10:58:45 +0200

    -edit: script names

--- javascript/feedback.js
+++ javascript/feedback.js
@@ -1,10 +1,12 @@
-// //STARS RATING
+//STARS RATING
 const stars = document.querySelectorAll(".ratingStars img");
 
 stars.forEach((star, index1) => {
   star.addEventListener("click", () => {
     stars.forEach((star, index2) => {
-      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+      index1 >= index2
+        ? star.classList.add("active")
+        : star.classList.remove("active");
     });
   });
 });
