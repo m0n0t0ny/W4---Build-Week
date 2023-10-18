1c56b7b3dd4909cde7392a26cc008ecc7b877edd
Chiara Puleio <chiarapuleio@yahoo.it>
2023-10-18 17:55:40 +0200

    JS Test Page
    function to compare the answers added

--- javascript/test.js
+++ javascript/test.js
@@ -5,11 +5,7 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit"
-    ]
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
   },
   {
     category: "Science: Computers",
@@ -18,7 +14,7 @@ const questions = [
     question:
       "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
     correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"]
+    incorrect_answers: ["Static", "Private", "Public"],
   },
   {
     category: "Science: Computers",
@@ -26,25 +22,23 @@ const questions = [
     difficulty: "easy",
     question: "The logo for Snapchat is a Bell.",
     correct_answer: "False",
-    incorrect_answers: ["True"]
+    incorrect_answers: ["True"],
   },
   {
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
-    incorrect_answers: ["True"]
+    incorrect_answers: ["True"],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"]
+    incorrect_answers: [".png", ".jpeg", ".gif"],
   },
   {
     category: "Science: Computers",
@@ -52,20 +46,15 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet"
-    ]
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the code name for the mobile operating system Android 7.0?",
+    question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"]
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
   {
     category: "Science: Computers",
@@ -73,7 +62,7 @@ const questions = [
     difficulty: "easy",
     question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"]
+    incorrect_answers: ["120", "160", "100"],
   },
   {
     category: "Science: Computers",
@@ -81,17 +70,16 @@ const questions = [
     difficulty: "easy",
     question: "Linux was first created as an alternative to Windows XP.",
     correct_answer: "False",
-    incorrect_answers: ["True"]
+    incorrect_answers: ["True"],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "Which programming language shares its name with an island in Indonesia?",
+    question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"]
-  }
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
 ];
 
 // QUIZ ✅
@@ -110,9 +98,7 @@ function loadQuestions() {
   h2.innerText = questions[currentQuestion].question;
   answers.innerHTML = "";
   const question = questions[currentQuestion];
-  const totalAnswers = question.incorrect_answers.concat(
-    question.correct_answer
-  );
+  const totalAnswers = question.incorrect_answers.concat(question.correct_answer);
 
   for (let i = 0; i < totalAnswers.length; i++) {
     const answersDiv = document.createElement("div");
@@ -133,9 +119,7 @@ function loadQuestions() {
     answers.appendChild(answersDiv);
 
     const questionCounter = document.getElementById("question-counter");
-    questionCounter.innerHTML = `<p>QUESTION &nbsp;${
-      currentQuestion + 1
-    } <span>/ ${questions.length}</span></p>`;
+    questionCounter.innerHTML = `<p>QUESTION &nbsp;${currentQuestion + 1} <span>/ ${questions.length}</span></p>`;
   }
 
   const answerButton = document.querySelectorAll("input");
@@ -178,7 +162,24 @@ function nextQuestion() {
     document.getElementById("answer-box").remove();
     document.getElementById("question-box").remove();
     document.getElementById("next-question").remove();
+    getScore();
     window.location.href = "results.html";
   }
   console.log("givenAnswers:", givenAnswers);
 }
+
+//GET YOUR SCORE
+const correctAnswers = [];
+const incorrectAnswers = [];
+
+function getScore() {
+  for (let i = 0; i < questions.length; i++) {
+    if (givenAnswers[i] === questions[i].correct_answer) {
+      correctAnswers.push();
+    } else {
+      incorrectAnswers.push();
+    }
+  }
+  // return correctAnswers.length
+}
+console.log(correctAnswers.length);
